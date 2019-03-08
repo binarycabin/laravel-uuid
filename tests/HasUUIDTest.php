c1c163729629a1b6a178e121ba4a2329ee30cbb1
Jeff Kilroy <jeffkilroy@gmail.com>
2019-03-07 21:51:09 -0500

    Added additional testing and doc files

---
+++ tests/HasUUIDTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BinaryCabin\Options\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HasUUIDTest extends TestCase
+{
+
+    public function test_coming_soon()
+    {
+        $this->assertTrue(true);
+    }
+
+}
